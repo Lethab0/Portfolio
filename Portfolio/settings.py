8405d85107e49c1eb5793f3b60cefa51a0ad96db
Lethab0 <lee.abiot@gmail.com>
2024-03-16 15:40:53 +0200

    config for heroku

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +146,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
